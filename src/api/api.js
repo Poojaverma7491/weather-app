d1afb13a6bd7a068a03f65c79e63f123fc5e4236
Pooja Verma <poojaverma97491@gmail.com>
2024-05-05 02:12:27 +0530

    first commit

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = '85eb8c53918ac5623065bd517c1ffda4';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
